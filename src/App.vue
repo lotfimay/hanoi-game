8702533ee665ab436c7cf5f72ec86ff104059ed5
Lotfi Mayouf <lotfimayouf09@gmail.com>
2022-12-22 21:24:20 +0100

    version 1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <hanoi-game />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HanoiGame from './components/HanoiGame.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HanoiGame,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700;800&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Nunito', sans-serif;
+}
+
+button {
+  cursor: pointer;
+}
+p {
+  font-weight: bold;
+  font-size: 1.2rem;
 }
 </style>
